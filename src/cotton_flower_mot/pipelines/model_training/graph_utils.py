Fix issue with similarity calculation.
I'll add a test for this later.

src/cotton_flower_mot/pipelines/model_training/graph_utils.py
@@ -46,8 +46,8 @@ def compute_bipartite_edge_features(
         left_node_indices = tf.range(0, limit=num_left_nodes)
         right_node_indices = tf.range(0, limit=num_right_nodes)
 
-        index_pair_left, index_pair_right = tf.meshgrid(
-            left_node_indices, right_node_indices
+        index_pair_right, index_pair_left = tf.meshgrid(
+            right_node_indices, left_node_indices
         )
         index_pair_left = tf.reshape(index_pair_left, (-1,))
         index_pair_right = tf.reshape(index_pair_right, (-1,))

src/cotton_flower_mot/pipelines/model_training/similarity_utils.py
@@ -70,7 +70,7 @@ def compute_ious(boxes1: tf.Tensor, boxes2: tf.Tensor) -> tf.Tensor:
     # Figure out the area.
     intersection_area = tf.reduce_prod(intersection, axis=1)
     union_area = tf.reduce_prod(union, axis=1)
-    return intersection_area / (union_area + _EPSILON)
+    return intersection_area / (tf.maximum(union_area, _EPSILON))
 
 
 def cosine_similarity(features1: tf.Tensor, features2: tf.Tensor) -> tf.Tensor: