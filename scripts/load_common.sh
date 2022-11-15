4a1bde099b0bdeb25de4c8866dd5653e12e07864
Daniel Petti <djpetti@gmail.com>
2022-11-15 16:05:50 -0500

    Disable TCMalloc for now.
    This seems to be causing OOM errors and allocation failures on GACRC.

--- scripts/load_common.sh
+++ scripts/load_common.sh
@@ -8,6 +8,6 @@ ml gperftools
 # https://keras.io/getting_started/faq/#how-can-i-obtain-reproducible-results-using-keras-during-development
 export PYTHONHASHSEED=0
 # Support for TCMalloc.
-export LD_PRELOAD="/apps/eb/gperftools/2.7.90-GCCcore-8.3.0/lib/libtcmalloc_minimal.so.4"
+#export LD_PRELOAD="/apps/eb/gperftools/2.7.90-GCCcore-8.3.0/lib/libtcmalloc_minimal.so.4"
 # Support for libdevice.
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/apps/eb/CUDAcore/11.2.1/
