WIP on numerical issues fixes.

src/cotton_flower_mot/pipelines/dataset_io.py
@@ -688,7 +688,7 @@ def _get_geometric_features(
 
         # Compute the offset.
         center_points_px = tf.stack([center_x, center_y], axis=1)
-        down_sample_factor = tf.constant(2 ** config.num_reduction_stages)
+        down_sample_factor = tf.constant(2**config.num_reduction_stages)
         offsets = (
             tf.cast(tf.round(center_points_px), tf.int32) % down_sample_factor
         )

src/cotton_flower_mot/pipelines/model_training/gcnn_model.py
@@ -6,6 +6,7 @@ https://arxiv.org/pdf/2010.00067.pdf
 from functools import partial
 from typing import Tuple
 
+import keras.backend
 import tensorflow as tf
 from loguru import logger
 from spektral.utils.convolution import line_graph
@@ -224,24 +225,44 @@ def _build_gnn(
         `[batch_size, n_nodes, n_gcn_channels]`.
 
     """
+    node_features = tf.debugging.assert_all_finite(
+        node_features, "node_features"
+    )
+    edge_features = tf.debugging.assert_all_finite(
+        edge_features, "edge_features"
+    )
+
     node_features = layers.BatchNormalization()(node_features)
     edge_features = layers.BatchNormalization()(edge_features)
 
+    l2 = keras.regularizers.L2(0.01)
+    regularizers = dict(
+        kernel_regularizer=l2,
+        node_regularizer=l2,
+        edge_regularizer=l2,
+        bias_regularizer=l2,
+        activity_regularizer=l2
+    )
     nodes1_1, edges1_1 = ResidualCensNet(
         config.num_node_features,
         config.num_edge_features,
         activation="relu",
+        **regularizers,
     )((node_features, graph_structure, edge_features))
 
+    edges1_1 = keras.backend.print_tensor(
+        edges1_1, summarize=-1
+    )
     nodes1_1 = tf.debugging.assert_all_finite(nodes1_1, "nodes1_1")
     edges1_1 = tf.debugging.assert_all_finite(edges1_1, "edges1_1")
 
     nodes1_1 = layers.BatchNormalization()(nodes1_1)
     edges1_1 = layers.BatchNormalization()(edges1_1)
-    nodes1_2, _ = ResidualCensNet(
+    nodes1_2, edges1_2 = ResidualCensNet(
         config.num_node_features,
         config.num_edge_features,
         activation="relu",
+        **regularizers,
     )((nodes1_1, graph_structure, edges1_1))
 
     return nodes1_2
@@ -501,6 +522,9 @@ def extract_interaction_features(
         appearance_features=(detections_app_features, tracklets_app_features),
         config=config,
     )
+    edge_features = tf.debugging.assert_all_finite(
+        edge_features, "edge_features"
+    )
 
     # Create the adjacency matrix and build the GCN.
     num_detections = detections_geometry.row_lengths()
@@ -526,6 +550,15 @@ def extract_interaction_features(
         node_features=combined_app_features,
         edge_features=edge_features,
         config=config,
+    )
+
+    # Limit the range on the interaction features. Otherwise, I tend to get
+    # issues with infinite values.
+    final_node_features = tf.clip_by_value(final_node_features, -100, 100)
+    final_node_features = tf.where(
+        tf.math.is_nan(final_node_features),
+        tf.zeros_like(final_node_features),
+        final_node_features,
     )
 
     # Split back into separate tracklets and detections.

src/cotton_flower_mot/pipelines/model_training/nodes.py
@@ -175,7 +175,7 @@ def train_model(
     geometry_loss_weight: float = 1.0,
     sinkhorn_loss_weight: float = 1.0,
     **kwargs: Any,
-) -> tf.keras.Model:
+) -> Tuple[tf.keras.Model, tf.keras.Model]:
     """
     Trains a model.
 
@@ -195,7 +195,7 @@ def train_model(
         **kwargs: Will be forwarded to `_make_callbacks()`.
 
     Returns:
-        The trained model.
+        The trained model from the best epoch and from the last epoch.
 
     """
     # Add a callback for keeping track of the best model.
@@ -234,4 +234,4 @@ def train_model(
             validation_freq=validation_frequency,
         )
 
-    return best_model_callback.best_model
+    return best_model_callback.best_model, model