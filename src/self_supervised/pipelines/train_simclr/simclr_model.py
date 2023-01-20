e4046cb0ba6e1ec29c37402559041db52ff6f2b6
Daniel Petti <djpetti@gmail.com>
2023-01-20 17:02:57 -0500

    WIP on SimCLR

--- src/self_supervised/pipelines/train_simclr/simclr_model.py
+++ src/self_supervised/pipelines/train_simclr/simclr_model.py
@@ -6,6 +6,8 @@ Implements the SimCLR-specific portions of the model.
 from torch import nn
 from torch import Tensor
 from .losses import NtXentLoss
+from typing import Any
+from torchvision.models import convnext_small
 
 
 class ProjectionHead(nn.Module):
@@ -34,8 +36,14 @@ class SimClrModel(nn.Module):
     Class that encapsulates the entire SimCLR model.
     """
 
-    def __init__(self, *, encoder: nn.Module, loss: NtXentLoss,
-                 num_features: int = 2048):
+    def __init__(
+        self,
+        *,
+        encoder: nn.Module,
+        loss: NtXentLoss,
+        num_features: int = 2048,
+        num_projected_outputs: int = 256
+    ):
         """
         Args:
             encoder: This is `f()` in the paper. It will be applied to
@@ -43,12 +51,16 @@ class SimClrModel(nn.Module):
             loss: The loss function to use.
             num_features: The number of features that we expect to be produced
                 by the encoder.
+            num_projected_outputs: Output size of the projection head.
 
         """
         super().__init__()
 
         self.encoder = encoder
         self.loss = loss
+        self.projection = ProjectionHead(
+            num_inputs=num_features, num_outputs=num_projected_outputs
+        )
 
     def forward(self, left_inputs: Tensor, right_inputs: Tensor) -> Tensor:
         """
@@ -65,3 +77,30 @@ class SimClrModel(nn.Module):
         right_rep = self.encoder(right_inputs)
 
         # Apply the projection.
+        left_projected = self.projection(left_rep)
+        right_projected = self.projection(right_rep)
+
+        # Apply the loss.
+        return self.loss(left_projected, right_projected)
+
+
+class ConvNeXtSmallEncoder(nn.Module):
+    """
+    Encoder module that uses ConvNeXt-S.
+    """
+
+    def __init__(self, *args: Any, num_features: int = 2048, **kwargs: Any):
+        """
+        Args:
+            *args: Will be forwarded to the ConvNeXt builder.
+            num_features: Number of output features to use.
+            **kwargs: Will be forwarded to the ConNeXt builder.
+
+        """
+        self.convnext = convnext_small()
+        # Internal projection head used to get the right number of output
+        # features for the representation.
+        self.projection = nn.Conv2d(768, num_features, (1, 1), padding="same")
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        return self.projection(self.convnext(inputs))
