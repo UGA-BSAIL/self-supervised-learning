a047fae28adcb6ee5759c99fd995c7041853078b
Daniel Petti <djpetti@gmail.com>
2023-01-30 09:35:36 -0500

    Fix issue with saving Torch models.
    Probably the right way to do this is to make an "official" version
    of `TensorFlowModelDataSet` that saves Torch models instead, but I
    haven't had time to do that.

--- src/self_supervised/data_sets/pytorch_model_data_set.py
+++ src/self_supervised/data_sets/pytorch_model_data_set.py
@@ -6,7 +6,7 @@ Loads/stores Pytorch models.
 import torch
 from kedro.io import AbstractVersionedDataSet
 from kedro.io import Version
-from pathlib import PurePosixPath
+from pathlib import PurePosixPath, Path
 from typing import Optional, Dict, Any
 from torch import nn
 
@@ -41,6 +41,10 @@ class PytorchModelDataSet(AbstractVersionedDataSet[nn.Module, nn.Module]):
             model: The model to save.
 
         """
+        parent_dir = Path(self._get_save_path()).parent
+        # Create parent directories if needed.
+        parent_dir.mkdir(parents=True, exist_ok=True)
+
         torch.save(model, self._get_save_path().as_posix())
 
     def _describe(self) -> Dict[str, Any]:
