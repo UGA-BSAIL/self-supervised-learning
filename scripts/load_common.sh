eb6c3ca49e62f725f738c44efae50b85c294a95b
Daniel Petti <djpetti@gmail.com>
2023-03-03 17:02:01 -0500

    Update to Python 3.10.
    It turns out that HPG doesn't support 3.9.

--- scripts/load_common.sh
+++ scripts/load_common.sh
@@ -1,5 +1,5 @@
 # Load needed modules.
-ml python/3.8
+ml python/3.10
 
 # Set this for deterministic runs. For more info, see
 # https://keras.io/getting_started/faq/#how-can-i-obtain-reproducible-results-using-keras-during-development
