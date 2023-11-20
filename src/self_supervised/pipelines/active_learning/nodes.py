6e13d7543ec683263dc23c324a087bf279c635f9
Daniel Petti <djpetti@gmail.com>
2023-11-20 11:59:41 -0500

    WIP on active learning.

--- src/self_supervised/pipelines/active_learning/nodes.py
+++ src/self_supervised/pipelines/active_learning/nodes.py
@@ -1,9 +1,14 @@
 from pathlib import Path
+from typing import Callable, Dict
+from functools import partial
 
 import torch
 from torch import nn
 from torchvision.io import read_image
 from torchvision.transforms.functional import resize
+import pandas as pd
+
+from ..frame_selector import FrameSelector
 
 
 def _get_image_rep(image_path: Path, *, model: nn.Module) -> torch.Tensor:
@@ -26,3 +31,35 @@ def _get_image_rep(image_path: Path, *, model: nn.Module) -> torch.Tensor:
     image_ex = image[None, :, :, :]
 
     return model(image_ex)[0].detach().reshape((1, -1))
+
+
+def save_image_reps(
+    *, metadata: pd.DataFrame, root_path: Path, model: nn.Module
+) -> Dict[str, Callable[[], pd.DataFrame]]:
+    """
+    Computes the image representations for all the images in a dataset.
+
+    Args:
+        metadata: The metadata for the MARS dataset.
+        root_path: The root path of the dataset.
+        model: The model to use for extracting representations.
+
+    Returns:
+        The dataset containing the image representations.
+
+    """
+    frame_selector = FrameSelector(mars_metadata=metadata)
+
+    def _get_reps(index: int) -> pd.DataFrame:
+        view_ids = frame_selector.get_all_views(index)
+        # Get the image reps for all views.
+        view_reps = [
+            _get_image_rep(root_path / v, model=model) for v in view_ids
+        ]
+
+        return pd.DataFrame(data=view_reps)
+
+    partitions = {}
+    for i in range(frame_selector.num_frames):
+        partitions[f"frame_{i}"] = partial(_get_reps, i)
+    return partitions
--- src/self_supervised/pipelines/active_learning/pipeline.py
+++ src/self_supervised/pipelines/active_learning/pipeline.py
@@ -1,10 +1,24 @@
 """
-This is a boilerplate pipeline 'active_learning'
-generated using Kedro 0.18.5
+Pipeline for active learning. It orders the images in a somewhat-optimal
+order for labelling.
 """
 
+
 from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import save_image_reps
 
 
-def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+def create_pipeline(**_) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                save_image_reps,
+                dict(
+                    metadata="mars_dataset_meta",
+                    root_folder="params:mars_image_folder",
+                    model="trained_model",
+                ),
+                "mars_image_reps",
+            )
+        ]
+    )
