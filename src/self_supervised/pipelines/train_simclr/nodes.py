07d8f74fb9b97d971bc4db989e4bd776af0621c6
Daniel Petti <djpetti@gmail.com>
2024-04-25 13:19:03 -0400

    Do an initial region update with just excess green.

--- src/self_supervised/pipelines/train_simclr/nodes.py
+++ src/self_supervised/pipelines/train_simclr/nodes.py
@@ -488,6 +488,10 @@ def train_model(
     if contrastive_crop:
         logger.debug("Updating regions on epochs {}", region_update_epochs)
 
+    # Do an initial region update with just excess green.
+    crop.update_regions(
+        representation_model, single_frame_loader, use_activations=False
+    )
     for i in range(num_epochs):
         logger.info("Starting epoch {}...", i)
         if contrastive_crop and i in region_update_epochs:
